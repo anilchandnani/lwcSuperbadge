e678de914caeb5368d0f7a9408527b1ca3ee3a61
Anil <anil.chandnani@outlook.com>
2021-03-08 00:09:48 +0530

    boat search results

--- force-app/main/default/lwc/boatSearchResults/boatSearchResults.js
+++ force-app/main/default/lwc/boatSearchResults/boatSearchResults.js
@@ -1,6 +1,7 @@
 import { LightningElement, api, wire } from 'lwc';
 import getBoats from '@salesforce/apex/BoatDataService.getBoats';
 import updateBoatList from '@salesforce/apex/BoatDataService.updateBoatList';
+import { updateRecord } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { refreshApex } from '@salesforce/apex';
 import { publish, MessageContext } from 'lightning/messageService';
@@ -17,7 +18,7 @@ const SUCCESS_VARIANT = 'success';
 const SUCCESS_TITLE = 'Success';
 const MESSAGE_SHIP_IT = 'Ship It!';
 const CONST_ERROR = 'Error';
-const ERROR_VARIANT ='error';
+const ERROR_VARIANT = 'error';
 
 export default class BoatSearchResults extends LightningElement {
     boats;
@@ -39,7 +40,7 @@ export default class BoatSearchResults extends LightningElement {
         if (result.error) {
 
         }
-        this.notifyLoading(false); 
+        this.notifyLoading(false);
     }
 
     updateSelectedTile(event) {
@@ -47,11 +48,14 @@ export default class BoatSearchResults extends LightningElement {
         this.sendMessageService(event.detail.boatId);
     }
 
-    handleSave(event) {
+    /*handleSave(event) {
+        this.notifyLoading(true);
         this.draftValues = event.detail.draftValues;
-        updateBoatList({
+        let singlePromises = updateBoatList({
             data: this.draftValues
-        })
+        });
+        let promises = [singlePromises];
+        Promise.all(promises)
             .then(result => {
                 const toastEvent = new ShowToastEvent({
                     "variant": SUCCESS_VARIANT,
@@ -64,26 +68,63 @@ export default class BoatSearchResults extends LightningElement {
             })
             .catch(error => {
                 const toastEvent = new ShowToastEvent({
-                    "variant": ERROR_VARIANT ,
+                    "variant": ERROR_VARIANT,
                     "title": CONST_ERROR,
                     "message": JSON.stringify(error)
                 }
                 );
                 this.dispatchEvent(toastEvent);
+                this.notifyLoading(false);
             }).finally(() => {
                 this.draftValues = [];
             });
+    }*/
+    handleSave(event) {
+        this.notifyLoading(true);
+       const recordInputs = event.detail.draftValues.slice().map(draft=>{
+           const fields = Object.assign({}, draft);
+           return {fields};
+       });
+
+       console.log(recordInputs);
+       const promises = recordInputs.map(recordInput => updateRecord(recordInput));
+       Promise.all(promises).then(res => {
+           this.dispatchEvent(
+               new ShowToastEvent({
+                   title: SUCCESS_TITLE,
+                   message: MESSAGE_SHIP_IT,
+                   variant: SUCCESS_VARIANT
+               })
+           );
+           this.draftValues = [];
+           return this.refresh();
+       }).catch(error => {
+           this.error = error;
+           this.dispatchEvent(
+                new ShowToastEvent({
+                    title: ERROR_TITLE,
+                    message: CONST_ERROR,
+                    variant: ERROR_VARIANT
+                })
+            );
+            this.notifyLoading(false);
+       }).finally(() => {
+            this.draftValues = [];
+        });
     }
 
-    async refresh(){
+    @api
+    async refresh() {
+        this.notifyLoading(true);
         await refreshApex(this.boats);
+        this.notifyLoading(false);
     }
 
-    sendMessageService(boatId){
-        publish(this.messageContext , BoatMC, {recordId : boatId});
+    sendMessageService(boatId) {
+        publish(this.messageContext, BoatMC, { recordId: boatId });
     }
 
-    notifyLoading(isLoading){
+    notifyLoading(isLoading) {
         this.dispatchEvent(new CustomEvent(isLoading ? 'loading' : 'doneloading'));
     }
 
